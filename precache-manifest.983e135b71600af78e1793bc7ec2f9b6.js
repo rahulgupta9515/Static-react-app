d9671365e4c6f49e19848b4d11f09994258356c8
Gupta, Rahul [PS] <rahul.gupta@qainfotech.com>
2019-08-30 22:33:16 +0530

    Updates

---
+++ precache-manifest.983e135b71600af78e1793bc7ec2f9b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694df93f0b66af9570b3cc33fb725921",
+    "url": "/Static-react-app/index.html"
+  },
+  {
+    "revision": "5eb5dabce144bcd8c6b7",
+    "url": "/Static-react-app/static/css/main.7ee120a6.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/Static-react-app/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "5eb5dabce144bcd8c6b7",
+    "url": "/Static-react-app/static/js/main.6c70727e.chunk.js"
+  },
+  {
+    "revision": "4daee1ebd4528b4fbaca",
+    "url": "/Static-react-app/static/js/runtime~main.0b18b26c.js"
+  }
+]);
